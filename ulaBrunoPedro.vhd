8d199960a81410cdb8b5c89e760d8b616c0dd5d9
unknown <pedropichelli04@gmail.com>
2025-09-12 15:27:25 -0300

    Usando unsigned em vez de vector

--- ulaBrunoPedro.vhd
+++ ulaBrunoPedro.vhd
@@ -1,12 +1,13 @@
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
 entity ulaBrunoPedro is
     port(
-        a       : in  std_logic_vector(15 downto 0);
-        b       : in  std_logic_vector(15 downto 0);
+        a       : in  unsigned(15 downto 0);
+        b       : in  unsigned(15 downto 0);
         controleoperacao : in  std_logic_vector(3 downto 0);
-        resultado : out std_logic_vector(15 downto 0);
+        resultado : out unsigned(15 downto 0);
         zero    : out std_logic
     );
 end ulaBrunoPedro;
