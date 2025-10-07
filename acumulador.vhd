332add72d3459f17bbe4a933000e7b4a3fbb5d5f
unknown <pedropichelli04@gmail.com>
2025-10-07 18:25:17 -0300

    Mudança no acumulador para se aproveitar do reg_16bits

--- acumulador.vhd
+++ acumulador.vhd
@@ -4,27 +4,35 @@ use ieee.numeric_std.all;
 
 entity acumulador is
     port (
-        clk   : in  std_logic;
-        rst   : in  std_logic;
-        write_enable  : in  std_logic;
-        data_in  : in  unsigned(15 downto 0);
-        data_out : out unsigned(15 downto 0)
+        clk          : in  std_logic;
+        rst          : in  std_logic;
+        write_enable : in  std_logic;
+        data_in      : in  unsigned(15 downto 0);
+        data_out     : out unsigned(15 downto 0)
     );
 end acumulador;
 
-architecture comportamento of acumulador is
-    signal reg : unsigned(15 downto 0) := (others => '0');
-begin
-    process(clk, rst)
-    begin
-        if rst = '1' then
-            reg <= (others => '0');
-        elsif rising_edge(clk) then
-            if write_enable = '1' then
-                reg <= data_in;
-            end if;
-        end if;
-    end process;
+architecture uso_reg of acumulador is
+    component reg_16bits
+        port (
+            clk      : in  std_logic;
+            rst      : in  std_logic;
+            wr_en    : in  std_logic;
+            data_in  : in  unsigned(15 downto 0);
+            data_out : out unsigned(15 downto 0)
+        );
+    end component;
 
-    data_out <= reg;
-end comportamento;
+    signal reg_out : unsigned(15 downto 0);
+begin
+    REG_INST : reg_16bits
+        port map (
+            clk      => clk,
+            rst      => rst,
+            wr_en    => write_enable,
+            data_in  => data_in,
+            data_out => reg_out
+        );
+
+    data_out <= reg_out;
+end uso_reg;
